e773c517981e6e73b1d5ab758f5208f56a65e00a
Sticks <sticks@teamhydra.dev>
2026-08-16 14:32:27 -0400

    fix config syntax

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({,
+export default defineNuxtConfig({
     devtools: { enabled: true },
 
     modules: [
